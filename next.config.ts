ae19df53fb5ee7dad0089ccaa16995ea90e10641
Anderson Araújo <andersoncmp1@gmail.com>
2025-10-29 20:09:26 +0000

    troque as imagens de alongamento start:https://imgur.com/Xmlk7Ua mobilid

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,'
         hostname: 'i.ytimg.com',
         port: '',
         pathname: '/**',
